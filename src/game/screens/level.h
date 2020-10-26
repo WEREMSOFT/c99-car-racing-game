8b3721022501712588584eda711b511146f74b4c
weremsoft <pablo.weremczuk@gmail.com>
2020-10-26 13:34:12 +0100

    [ADD] enemy cars

--- src/game/screens/level.h
+++ src/game/screens/level.h
@@ -11,6 +11,11 @@
 #define CAR_Z_POSITION 37.f
 #define TREE_COUNT 30
 #define TERRAIN_SIDE_SIZE 400.f
+#define CAR_INITIAL_SPEED 0.f
+#define CAR_ACCELERATION 10.f
+#define CAR_MAX_SPEED 150.f
+#define CAR_ENEMY_COUNT 5
+#define CAR_LENGTH 6.f
 
 typedef enum models_e {
     MODELS_TREE,
@@ -27,7 +32,10 @@ typedef struct level_t {
     Vector3 car_target_position;
     Vector3 car_position;
     Vector3 tree_positions[TREE_COUNT];
+    Vector3 car_enemy_position[CAR_ENEMY_COUNT];
+    float car_speed;
     float tree_offset;
+    int car_enemy_active_count;
     void (*update)(struct level_t* level);
 } level_t;
 
@@ -48,6 +56,8 @@ void level_pass_to_state_dead(level_t* level);
 level_t level_init(void){
     level_t return_value = {0};
  
+    return_value.car_enemy_active_count = 1;
+
     return_value.screen.camera = camera_init();
     
     return_value.car_target_position = return_value.car_position = (Vector3){0, 0, CAR_Z_POSITION};
@@ -55,12 +65,12 @@ level_t level_init(void){
 
     Image img_terrain = GenImageChecked(40, 40, 1, 1, DARKGRAY, GRAY);
     Image img_car = GenImageChecked(2, 2, 1, 1, PETROL, GRAY);
-    Image img_car_enemy = GenImageChecked(2, 2, 1, 1, BLUE, RED);
+    Image img_car_enemy = GenImageChecked(2, 2, 1, 1, ORANGE, RED);
     Image img_tree = GenImageChecked(2, 2, 1, 1, GREEN, DARKGREEN);
     
     return_value.models[MODELS_TERRAIN] = LoadModelFromMesh(GenMeshCube(TERRAIN_SIDE_SIZE, 0.2f, TERRAIN_SIDE_SIZE));
-    return_value.models[MODELS_CAR_HERO] = LoadModelFromMesh(GenMeshCube(4.f, 1.f, 6.f));
-    return_value.models[MODELS_CAR_ENEMY] = LoadModelFromMesh(GenMeshCube(4.f, 1.f, 6.f));
+    return_value.models[MODELS_CAR_HERO] = LoadModelFromMesh(GenMeshCube(4.f, 2.f, CAR_LENGTH));
+    return_value.models[MODELS_CAR_ENEMY] = LoadModelFromMesh(GenMeshCube(4.f, 2.f, CAR_LENGTH));
     return_value.models[MODELS_TREE] = LoadModelFromMesh(GenMeshCylinder(1.f, 5.f, 4));
 
     return_value.models[MODELS_TERRAIN].materials[0].maps[MAP_DIFFUSE].texture = LoadTextureFromImage(img_terrain);
@@ -78,6 +88,9 @@ level_t level_init(void){
         return_value.tree_positions[i].x = TREE_RADIUS;
     }
 
+    for(int i = 0; i < CAR_ENEMY_COUNT; i++)
+        return_value.car_enemy_position[i].z = TERRAIN_SIDE_SIZE / -2;
+
     level_pass_to_state_playing(&return_value);
     return return_value;
 }
@@ -92,7 +105,22 @@ void level_fini(level_t level) {
 
 static void process_state_playing(level_t* level){
 
-    level->terrain_position.z += 100.f * GetFrameTime();
+
+    if(level->car_enemy_active_count < CAR_ENEMY_COUNT && level->car_enemy_position[level->car_enemy_active_count - 1].z - level->car_enemy_position[level->car_enemy_active_count].z > CAR_LENGTH * 4)
+        level->car_enemy_active_count++;
+
+    level->car_speed += CAR_ACCELERATION * GetFrameTime();
+    level->car_speed = fmin(level->car_speed, CAR_MAX_SPEED);
+    level->terrain_position.z += level->car_speed * 1.4 * GetFrameTime();
+
+    for(int i = 0; i < level->car_enemy_active_count; i++){
+        level->car_enemy_position[i].z += level->car_speed * GetFrameTime();
+
+        if(level->car_enemy_position[i].z > CAR_Z_POSITION){
+            level->car_enemy_position[i].z -= TERRAIN_SIDE_SIZE / 2;
+            level->car_enemy_position[i].x = GetRandomValue(-1, 1) * CAR_LATERAL_SPEED;
+        }
+    }
 
     if(level->terrain_position.z > 20.f) 
         level->terrain_position.z -= 20.f;
@@ -106,6 +134,8 @@ static void process_state_playing(level_t* level){
         {
             DrawModel(level->models[MODELS_TERRAIN], level->terrain_position, 1.f, WHITE);
             DrawModel(level->models[MODELS_CAR_HERO], level->car_position, 1.f, WHITE);
+            for(int i = 0; i < level->car_enemy_active_count; i++)
+                DrawModel(level->models[MODELS_CAR_ENEMY], level->car_enemy_position[i], 1.f, WHITE);
 
             for(int i = 0; i < TREE_COUNT; i++){
                 level->tree_positions[i].z = i * 10 + level->terrain_position.z - 200;
